4f350cac79ef17ac1dd74eaff1326cac916e9581
jaden <irang1020@gmail.com>
2018-10-19 20:03:09 +0900

    initial commit

---
+++ my-cooc/src/main/java/com/coocmaster/MyCoocApplication.java
@@ -0,0 +1,12 @@
+package com.coocmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCoocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCoocApplication.class, args);
+	}
+}
